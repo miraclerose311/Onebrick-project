e78ac8a7eed2266299453e9f69a80d8e3c1cc43a
miraclerose311 <miraclerose311@gmail.com>
2023-11-30 16:46:23 +0100

    fix some bugs

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 // Define Routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
