4fbd44cc41bcaad4c5390aaa13bcafe46d11b566
miraclerose311 <miraclerose311@gmail.com>
2023-12-21 14:23:59 +0800

    Sold modal

--- client/src/components/modals/SoldModal.jsx
+++ client/src/components/modals/SoldModal.jsx
@@ -1,21 +1,23 @@
 import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 
 const SoldModal = ({ modalPosition, brick_id }) => {
+	const { amount } = useSelector((state) => state.brick.current);
 	return (
 		<div
-			className="border border-gray-600 rounded bg-gray-200 absolute px-4 py-8 w-52 h-64 flex flex-col justify-center items-center z-30"
+			className='border border-gray-600 rounded bg-gray-200 absolute px-4 py-8 w-52 h-64 flex flex-col justify-center items-center z-30'
 			style={{
 				left: modalPosition.x,
 				top: modalPosition.y,
 				boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
 			}}
 		>
-			<p className="text-md py-3 font-montserrat">Yay!!</p>
-			<p className="w-full text-xs pb-2">
-				You have paid for 1 Brick of Hope. Your Brick Credential is
+			<p className='text-md py-3 font-montserrat'>Yay!!</p>
+			<p className='w-full text-xs pb-2'>
+				You have paid for {amount} Brick of Hope. Your Brick Credential is
 			</p>
-			<p className="bg-yellow-500 text-sm px-12 py-2 my-3">{brick_id}</p>
-			<p className="text-xs">
+			<p className='bg-yellow-500 text-sm px-12 py-2 my-3'>{brick_id}</p>
+			<p className='text-xs'>
 				You can now locate your brick on the Wall of Hope by search using this
 				ID or your Name.
 			</p>
